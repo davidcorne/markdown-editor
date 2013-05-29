22119211b2c1f0b9301e2fbced3be73e6fa8e494
David Corne <davidcorne@gmail.com>
2013-05-29 17:02:48 +0100

    Read user text and tool tips from file

--- Configuration.py
+++ Configuration.py
@@ -39,14 +39,26 @@ def read_options():
         global OPTIONS
         OPTIONS = pickle.load(options_file)
 
-read_options()
-
 #==============================================================================
 def save_options():
     options_path = os.path.join(resource_dir(), options_file_name())
     with open(options_path, "wb") as options_file:
         pickle.dump(OPTIONS, options_file)
     
+#==============================================================================
+def read_user_text():
+    filename = os.path.join(resource_dir(), "UserText.pickle")
+    with open(filename, "rb") as user_text_file:
+        global USER_TEXT
+        USER_TEXT = pickle.load(user_text_file)
+
+#==============================================================================
+def read_tool_tips():
+    filename = os.path.join(resource_dir(), "ToolTips.pickle")
+    with open(filename, "rb") as tool_tips_file:
+        global TOOL_TIP
+        TOOL_TIP = pickle.load(tool_tips_file)
+
 MARKDOWN_FILE_STRING = """\
 Markdown (*.md *.markdown *.mdown *.mkdn *.mkd *.mdtxt *.mdtext *.text);;\
 All Files (*)\
@@ -59,112 +71,14 @@ Images (*.png *.jpg *.jpeg *.gif *.bmp *.emf *.wmf *.jfif *.jpe *.dib *.rle \
 All Files (*)\
 """
 
-USER_TEXT = {
-    "program_name": "MarkdownEditor",
-    "undo_redo_toolbar": "Undo Redo Toolbar",
-    "format_toolbar": "Format Toolbar",
-    "file_toolbar": "File Toolbar",
-    "edit_toolbar": "Edit Toolbar",
-    "edit_menu": "&Edit",
-    "tools_menu": "&Tools",
-    "file_menu": "&File",
-    "find_what": "Find &what:",
-    "match_case": "Match &case",
-    "match_whole_words": "Match &whole words",
-    "search_backwards": "Search &backwards",
-    "find_title": "Find",
-    "replace_title": "Replace",
-    "replace_with": "Replace with:",
-    "replace": "&Replace",
-    "replace_all": "Replace &all",
-    "find": "&Find",
-    "close": "Close",
-    "close_file": "Close File",
-    "options": "Options",
-    "copy" : "Copy",
-    "cut" : "Cut",
-    "paste" : "Paste",
-    "export_html" : "Export html",
-    "new_file" : "New File",
-    "open_file" : "Open File",
-    "undo" : "Undo",
-    "redo" : "Redo",
-    "save_file" : "Save",
-    "save_all" : "Save All Files",
-    "save_as" : "Save As",
-    "find_and_replace" : "Find and Replace",
-    "select_all" : "Select All",
-    "saved": "File saved",
-    "exception" : "An internal error has occured.",
-    "error_text_was": "The text from it was:",
-    "insert_link": "Insert Link",
-    "enter_link": "Enter the destination URL",
-    "insert_image": "Insert Image",
-    "enter_image_location": "Enter the image location",
-    "enter_image_title": "Enter the image title (optional)",
-    "browse_for_image": "Browse for image",
-    "current_document": "the current document",
-    "save_changes?": "Do you want to save your changes?",
-    "made_changes": "You have made changes to",
-    "markdown": "Markdown",
-    "markdown_extra": "Markdown Extra",
-    "github_flavoured_markdown": "Github Flavoured Markdown",
-    "show_html": "Show HTML",
-    "misc": "Miscellaneous",
-    "markdown_type": "Markdown Type",
-    "debug_options": "Debug Options",
-    "css": "CSS",
-    "style_name": "Style Name",
-    "code": "Code",
-    "preview": "Preview",
-    "print": "Print",
-    "print_markdown": "Print Markdown",
-    "print_rendered_html": "Print Rendered HTML",
-    "print_raw_html": "Print Raw HTML Code",
-    "print_preview_markdown": "Print Preview Markdown",
-    "print_preview_rendered_html": "Print Preview Rendered HTML",
-    "print_preview_raw_html": "Print Preview Raw HTML Code",
-    }
-
-TOOL_TIP = {
-    "bold": "Surround the highlighted area with strong emphasis (__)",
-    "italic": "Surround the highlighted area with emphasis (_)",
-    "code": "Surround the highlighted area with code blocks (```)",
-    "choose_colour": "Raise a colour chooser dialog",
-    "close_file": "Close the currently open document",
-    "configure": "Configure the program",
-    "copy": "Copy the highlighted area to the clipboard",
-    "cut": "Cut the highlighted area to the clipboard",
-    "export_html": "Export the current document as html",
-    "find_and_replace": "Raise the find and replace window",
-    "new_file": "Start a new document",
-    "open_file": "Open an existing document",
-    "paste": "Paste from the clipboard to the current cursor location",
-    "redo": "Redo the previous undo",
-    "save_all": "Save all open documents",
-    "save_as": "Save the current document with a specified name",
-    "save_file": "Save the current document",
-    "select_all": "Select all the text in the current document",
-    "set_colour": "Set the colour of the highlighted area to this colour",
-    "undo": "Undo the previous change",
-    "link": "Insert a link with the link text of the surrounded area",
-    "image": "Insert an image with the title of the surrounded area",
-    "print_menu": "Opens the printing menu",
-    "print_markdown": "Prints the markdown document",
-    "print_rendered_html": "Prints the HTML output as it will be rendered by a browser",
-    "print_raw_html": "Prints the raw HTML code output",
-    "print_preview_markdown": "Shows a preview of the markdown before printing",
-    "print_preview_rendered_html": "Shows a preview of the HTML output as it will be rendered by a browser before printing",
-    "print_preview_raw_html": "Shows a preview of the raw HTML code output before printing",
-    }
-
 #==============================================================================
 def find_images():
-    images = dict()
+    global IMAGES
+    IMAGES = dict()
     directory = os.path.join(resource_dir(), "Images")
     if (os.path.isdir(directory)):
         for image in os.listdir(directory):
-            images[os.path.splitext(image)[0]] = os.path.join(
+            IMAGES[os.path.splitext(image)[0]] = os.path.join(
                 directory, 
                 image
                 )
@@ -172,16 +86,6 @@ def find_images():
         message = "Image files not found."
         detail = "Images should be found at " + os.path.abspath(directory)
         Error.show_error(message, detail)
-    return images
-
-IMAGES = find_images()
-
-PROCESSOR_TYPES = collections.OrderedDict()
-PROCESSOR_TYPES["markdown"] = Processor.Markdown
-PROCESSOR_TYPES["markdown_extra"] = Processor.MarkdownExtra
-PROCESSOR_TYPES["github_flavoured_markdown"] = Processor.GithubFlavouredMarkdown
-
-PROCESSOR = PROCESSOR_TYPES[OPTIONS["processor"]]()
 
 #==============================================================================
 def read_css(filename):
@@ -207,8 +111,20 @@ def reload_code_css():
     global CODE_CSS
     CODE_CSS = css
 
+read_options()
+read_user_text()
+read_tool_tips()
+find_images()
+
 reload_markdown_css()
 reload_code_css()
+
+PROCESSOR_TYPES = collections.OrderedDict()
+PROCESSOR_TYPES["markdown"] = Processor.Markdown
+PROCESSOR_TYPES["markdown_extra"] = Processor.MarkdownExtra
+PROCESSOR_TYPES["github_flavoured_markdown"] = Processor.GithubFlavouredMarkdown
+
+PROCESSOR = PROCESSOR_TYPES[OPTIONS["processor"]]()
 
 #==============================================================================
 if (__name__ == "__main__"):
--- bin/write_options.py
+++ bin/write_options.py
@@ -21,15 +21,133 @@ OPTIONS = {
     "markdown_css": "Markdown"
     }
 
+USER_TEXT = {
+    "program_name": "MarkdownEditor",
+    "undo_redo_toolbar": "Undo Redo Toolbar",
+    "format_toolbar": "Format Toolbar",
+    "file_toolbar": "File Toolbar",
+    "edit_toolbar": "Edit Toolbar",
+    "edit_menu": "&Edit",
+    "tools_menu": "&Tools",
+    "file_menu": "&File",
+    "find_what": "Find &what:",
+    "match_case": "Match &case",
+    "match_whole_words": "Match &whole words",
+    "search_backwards": "Search &backwards",
+    "find_title": "Find",
+    "replace_title": "Replace",
+    "replace_with": "Replace with:",
+    "replace": "&Replace",
+    "replace_all": "Replace &all",
+    "find": "&Find",
+    "close": "Close",
+    "close_file": "Close File",
+    "options": "Options",
+    "copy" : "Copy",
+    "cut" : "Cut",
+    "paste" : "Paste",
+    "export_html" : "Export html",
+    "new_file" : "New File",
+    "open_file" : "Open File",
+    "undo" : "Undo",
+    "redo" : "Redo",
+    "save_file" : "Save",
+    "save_all" : "Save All Files",
+    "save_as" : "Save As",
+    "find_and_replace" : "Find and Replace",
+    "select_all" : "Select All",
+    "saved": "File saved",
+    "exception" : "An internal error has occured.",
+    "error_text_was": "The text from it was:",
+    "insert_link": "Insert Link",
+    "enter_link": "Enter the destination URL",
+    "insert_image": "Insert Image",
+    "enter_image_location": "Enter the image location",
+    "enter_image_title": "Enter the image title (optional)",
+    "browse_for_image": "Browse for image",
+    "current_document": "the current document",
+    "save_changes?": "Do you want to save your changes?",
+    "made_changes": "You have made changes to",
+    "markdown": "Markdown",
+    "markdown_extra": "Markdown Extra",
+    "github_flavoured_markdown": "Github Flavoured Markdown",
+    "show_html": "Show HTML",
+    "misc": "Miscellaneous",
+    "markdown_type": "Markdown Type",
+    "debug_options": "Debug Options",
+    "css": "CSS",
+    "style_name": "Style Name",
+    "code": "Code",
+    "preview": "Preview",
+    "print": "Print",
+    "print_markdown": "Print Markdown",
+    "print_rendered_html": "Print Rendered HTML",
+    "print_raw_html": "Print Raw HTML Code",
+    "print_preview_markdown": "Print Preview Markdown",
+    "print_preview_rendered_html": "Print Preview Rendered HTML",
+    "print_preview_raw_html": "Print Preview Raw HTML Code",
+    }
+
+TOOL_TIPS = {
+    "bold": "Surround the highlighted area with strong emphasis (__)",
+    "italic": "Surround the highlighted area with emphasis (_)",
+    "code": "Surround the highlighted area with code blocks (```)",
+    "choose_colour": "Raise a colour chooser dialog",
+    "close_file": "Close the currently open document",
+    "configure": "Configure the program",
+    "copy": "Copy the highlighted area to the clipboard",
+    "cut": "Cut the highlighted area to the clipboard",
+    "export_html": "Export the current document as html",
+    "find_and_replace": "Raise the find and replace window",
+    "new_file": "Start a new document",
+    "open_file": "Open an existing document",
+    "paste": "Paste from the clipboard to the current cursor location",
+    "redo": "Redo the previous undo",
+    "save_all": "Save all open documents",
+    "save_as": "Save the current document with a specified name",
+    "save_file": "Save the current document",
+    "select_all": "Select all the text in the current document",
+    "set_colour": "Set the colour of the highlighted area to this colour",
+    "undo": "Undo the previous change",
+    "link": "Insert a link with the link text of the surrounded area",
+    "image": "Insert an image with the title of the surrounded area",
+    "print_menu": "Opens the printing menu",
+    "print_markdown": "Prints the markdown document",
+    "print_rendered_html": "Prints the HTML output as it will be rendered by a browser",
+    "print_raw_html": "Prints the raw HTML code output",
+    "print_preview_markdown": "Shows a preview of the markdown before printing",
+    "print_preview_rendered_html": "Shows a preview of the HTML output as it will be rendered by a browser before printing",
+    "print_preview_raw_html": "Shows a preview of the raw HTML code output before printing",
+    }
+
 #==============================================================================
-def write_options_file():
+def write_config_file(object, file_name):
+    """
+    Pickles the object to file_name where file_name is a relative path under 
+    Resources
+    """
     options_path = os.path.join(
         os.path.dirname(sys.argv[0]),
-        "../Resources/Options.pickle"
+        "../Resources"
         )
-    with open(options_path, "wb") as options_file:
-        pickle.dump(OPTIONS, options_file)
+    file_path = os.path.join(options_path, file_name)
+    with open(file_path, "wb") as options_file:
+        pickle.dump(object, options_file)
+
+#==============================================================================
+def write_options_file():
+    write_config_file(OPTIONS, "Options.pickle")
+    
+#==============================================================================
+def write_user_text():
+    write_config_file(USER_TEXT, "UserText.pickle")
+    
+#==============================================================================
+def write_tool_tips():
+    write_config_file(TOOL_TIPS, "ToolTips.pickle")
     
 #==============================================================================
 if (__name__ == "__main__"):
     write_options_file()
+    write_user_text()
+    write_tool_tips()
