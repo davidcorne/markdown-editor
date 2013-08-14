959f68166680c9c23d5fd51020f0d22612271d89
David Corne <davidcorne@gmail.com>
2013-08-14 16:23:18 +0100

    Guard against UserText.pickle not being found.

--- Error.py
+++ Error.py
@@ -6,12 +6,36 @@ import sys
 import traceback
 
 # local imports
+import Resources
 
-from UserText import USER_TEXT
+#==============================================================================
+def get_user_text():
+    """
+    This is done here in case the user text file is not found.
+    """
+    try:
+        from UserText import USER_TEXT
+    except IOError:
+        # The user text file has not been found, this is doing quite a lot in
+        # an except block, but this SHOULD be safe.
+        USER_TEXT = {
+            "exception": """\
+There has been an installation error.
+
+Cannot find localisation file:
+  %s
+
+Please report this at https://bitbucket.org/davidcorne/markdown-editor/issues
+
+Sorry for any inconvenience.
+""" %(Resources.directory()),
+            "program_name": "MarkdownEditor",
+            }
+    return USER_TEXT
 
 #==============================================================================
 def exception_hook(exception_type, exception_value, trace):
-    message = USER_TEXT["exception"]
+    message = get_user_text()["exception"]
     detail = traceback.format_exception(
         exception_type,
         exception_value,
@@ -29,7 +53,7 @@ def show_error(message, detail=None):
     if (not QtGui.QApplication.instance()):
         app = QtGui.QApplication(sys.argv)
     message_box = QtGui.QMessageBox()
-    message_box.setWindowTitle(USER_TEXT["program_name"])
+    message_box.setWindowTitle(get_user_text()["program_name"])
     message_box.setIcon(QtGui.QMessageBox.Critical)
     message_box.addButton(QtGui.QMessageBox.Ok)
     message_box.setText(message)
@@ -43,9 +67,10 @@ def tk_quit():
     The only reason you should get here is an import error from Qt.
     """
     import tkMessageBox
+    user_text = get_user_text()
     tkMessageBox.showerror(
-        USER_TEXT["program_name"],
-        USER_TEXT["user_interface_import_fail"]
+        user_text["program_name"],
+        user_text["user_interface_import_fail"]
         )
     sys.exit()
 
