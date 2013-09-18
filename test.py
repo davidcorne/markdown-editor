7f3098092dd3d302990414daa8b37bf91e2ca2f6
David Corne <davidcorne@gmail.com>
2013-09-18 13:27:47 +0100

    Remove dependency on nosetest, use python module to run all tests.

---
+++ test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# Written by: DGC
+
+# python imports
+import unittest
+
+# local imports
+from utest_ImageConverter import *
+from utest_Processor import *
+from utest_Updater import *
+
+#==============================================================================
+if (__name__ == "__main__"):
+    unittest.main(verbosity=2)
