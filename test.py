14670831d741e5b2d7fc42e2dbd74e7f63ff1444
David Corne <davidcorne@gmail.com>
2013-10-14 14:52:53 +0100

    Make test.py work

--- test.py
+++ test.py
@@ -2,9 +2,11 @@
 # Written by: DGC
 
 # python imports
+import sys
 import unittest
 
 # local imports
+sys.path.append("utest")
 from utest_ImageConverter import *
 from utest_Processor import *
 from utest_Updater import *
