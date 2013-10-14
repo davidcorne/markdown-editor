c818882ab2a0daa065bc57dfa710174711898093
David Corne <davidcorne@gmail.com>
2013-10-14 15:38:40 +0100

    Make utest_document work on it's own
    --HG--
    branch : Rationalise Document

--- utest/utest_Document.py
+++ utest/utest_Document.py
@@ -2,9 +2,11 @@
 # Written by: DGC
 
 # python imports
+import sys
 import unittest
 
 # local imports
+sys.path.append("..")
 from Document import *
 
 #==============================================================================
