74c7dad866e494226c4aa329d60dd379f07fd754
David Corne <davidcorne@gmail.com>
2013-10-14 15:56:08 +0100

    Add document blank functions and add passing test
    --HG--
    branch : Rationalise Document

--- Document.py
+++ Document.py
@@ -4,6 +4,8 @@
 # python imports
 from __future__ import unicode_literals
 
+import os
+
 # local imports
 
 #==============================================================================
@@ -19,6 +21,31 @@ class Document(object):
         if (file_path):
             with open(self.file_path, "r") as markdown_file:
                 self.content = markdown_file.read()
+
+    @property
+    def filename(self):
+        if (self.file_path is not None):
+            return os.path.basename(unicode(self.file_path))
+
+    def save_file(self):
+        with open(self.file_path, "w") as text_file:
+            text_file.write(self.content)
+        self.saved = True
+
+    def colour(self, beginning, end):
+        pass
+
+    def bold(self, beginning, end):
+        pass
+
+    def italic(self, beginning, end):
+        pass
+
+    def code(self, beginning, end):
+        pass
+
+    def insert_link(self, beginning, end):
+        pass
 
 #==============================================================================
 if (__name__ == "__main__"):
--- utest/utest_Document.py
+++ utest/utest_Document.py
@@ -2,10 +2,12 @@
 # Written by: DGC
 
 # python imports
+import os.path
 import sys
 import unittest
 
 # local imports
+import test_utils
 sys.path.append("..")
 from Document import *
 
@@ -18,6 +20,19 @@ class utest_Document(unittest.TestCase):
         # use __file__ so it's a real file
         doc = Document(__file__)
         self.assertTrue(doc.saved)
+
+    def test_sample_markdown(self):
+        path = test_utils.data_dir() + "/Markdown/Sample.md"
+        assert(os.path.isfile(path))
+        doc = Document(path)
+        self.assertTrue(doc.saved)
+        self.assertEqual(
+            doc.content,
+            """# Sample #
+
+A sample of markdown.
+"""
+            )
 
 #==============================================================================
 if (__name__ == "__main__"):
