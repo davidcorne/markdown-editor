d06a82c0505eeb2079a5849b1aba3aca100f84a6
David Corne <davidcorne@gmail.com>
2013-10-14 14:55:01 +0100

    Make test.py work better by making utest a package

--- test.py
+++ test.py
@@ -2,15 +2,13 @@
 # Written by: DGC
 
 # python imports
-import sys
 import unittest
 
 # local imports
-sys.path.append("utest")
-from utest_ImageConverter import *
-from utest_Processor import *
-from utest_Updater import *
-from utest_html import *
+from utest.utest_ImageConverter import *
+from utest.utest_Processor import *
+from utest.utest_Updater import *
+from utest.utest_html import *
 
 #==============================================================================
 if (__name__ == "__main__"):
