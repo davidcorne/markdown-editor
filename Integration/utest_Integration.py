1d1e6d0bb9f1e4445dea86ab692f0b9e546f9cf7
David Corne <davidcorne@gmail.com>
2013-10-18 15:09:18 +0100

    Add more tabs to the integration test.

--- Integration/utest_Integration.py
+++ Integration/utest_Integration.py
@@ -46,11 +46,41 @@ This is a test of markdown.
         self.assertEqual(html.count("<p>"), 1)
         self.assertEqual(html.count("</p>"), 1)
 
-    def test_close_tab(self):
+    def test_close_tabs(self):
         app = MarkdownEditor.MarkdownEditorApp([])
         editor = MarkdownEditor.MarkdownEditor([])
         editor.new_file()
         editor.tab_close_requested(0)
+        self.assertEqual(editor.editor.count(), 0)
+        
+        # make 6 new tabs
+        editor.new_file()
+        editor.new_file()
+        editor.new_file()
+        editor.new_file()
+        editor.new_file()
+        editor.new_file()
+        self.assertEqual(editor.editor.count(), 6)
+        # we are on the last tab
+        self.assertEqual(editor.editor.currentIndex(), 5)
+        editor.tab_close_requested(0)
+        # close the first tab, we are on index 4
+        self.assertEqual(editor.editor.currentIndex(), 4)
+        # more to tab index 2
+        editor.editor.setCurrentIndex(2)
+        editor.tab_close_requested(4)
+        # close a tab to the right, our index doesn't change
+        self.assertEqual(editor.editor.currentIndex(), 2)
+        # close a tab to our left, our index decrements
+        editor.tab_close_requested(0)
+        self.assertEqual(editor.editor.currentIndex(), 1)
+        editor.tab_close_requested(1)
+        # close our tab, if there are tabs to the right our index is the same
+        self.assertEqual(editor.editor.currentIndex(), 1)
+        editor.tab_close_requested(1)
+        self.assertEqual(editor.editor.currentIndex(), 0)
+        editor.tab_close_requested(0)
+        # we have deleted them all
         self.assertEqual(editor.editor.count(), 0)
 
 #==============================================================================
